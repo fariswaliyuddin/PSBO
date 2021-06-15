f370cd9f3a04aa30184a172816997e9746528024
fariswaliyuddin <60166714+fariswaliyuddin@users.noreply.github.com>
2021-06-15 22:20:16 +0700

    backend belum
    Perkembangan terkini

---
+++ Frontend/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express();
+
+// Make sure you place body-parser before your CRUD handlers!
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+//Halaman Awal(login)
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/', (req, res) => {
+})
+
+//Register
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+})
+
+//Menus
+app.get('/menus', (req, res) => {
+    res.render('menus.ejs')
+})
+
+//Restaurant
+app.get('/restaurants', (req, res) =>{
+    res.render('restaurants.ejs')
+})
+
+
+//Start Server
+app.listen(3000, function(){    
+    console.log('server start at 3000')
+})
