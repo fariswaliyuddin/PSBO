65f1e177a4ef9afc2e3d7d4d42d8cd284e8f3c92
fariswaliyuddin <60166714+fariswaliyuddin@users.noreply.github.com>
2021-06-16 08:31:11 +0700

    login register
    login register bisa

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,33 +1,90 @@
 const express = require('express')
-const app = express();
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
+
 
 // Make sure you place body-parser before your CRUD handlers!
 app.use( express.static( "public" ) );
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-//Halaman Awal(login)
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
 
-app.post('/', (req, res) => {
+app.get('/menus', checkAuthenticated, (req, res) => {
     res.render('menus.ejs')
-})
-
-//Register
-app.get('/register', (req, res) => {
+  })
+  
+  app.get('/', checkNotAuthenticated, (req, res) => {
+    res.render('index.ejs')
+  })
+  
+  app.post('/', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/menus',
+    failureRedirect: '/',
+    failureFlash: true
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
-})
-
-app.post('/register', (req, res) => {
-    res.render('index.ejs')
-})
-
-//Menus
-app.get('/menus', (req, res) => {
-    res.render('menus.ejs')
-})
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        birthdate: req.body.birthdate,
+        gender: req.body.gender
+      })
+      res.redirect('/')
+    } catch {
+      res.redirect('/register')
+    }
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/menus')
+    }
+    next()
+  }
 
 //Diary
 app.get('/diary', (req, res) =>{
